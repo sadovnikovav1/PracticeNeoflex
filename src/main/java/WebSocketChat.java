2fc8dd59e957d5c7c7975114dd3e63a76a51b6ec
sadovnikovav1 <sadovnikovav1@gmail.com>
2017-07-09 20:24:37 +0400

    added apache tomcat server

---
+++ src/main/java/WebSocketChat.java
@@ -0,0 +1,19 @@
+package main.java;
+
+/**
+ * Created by Алексей on 09.07.2017.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebSocketChat {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
